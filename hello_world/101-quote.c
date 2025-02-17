172a57abce0ddd8ea2510c16e9588e2060c9e861
Josephine Dang <dangjos02@gmail.com>
2025-02-17 08:02:08 +0000

    fix betty errors

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -5,13 +5,9 @@
  * Return: 1
  */
 #include <unistd.h>
-
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-    write(2, message, 59);  
-
-    return 1;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
 }
 
