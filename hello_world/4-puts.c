fdf34db52ffad96720c7edbaa32f460b0209ad61
Josephine Dang <dangjos02@gmail.com>
2025-02-17 05:59:50 +0000

    C - script 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a message using puts.
+ * Return: 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
