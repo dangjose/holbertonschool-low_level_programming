34287807c0541ddc1da865056f44c27cab3e8448
Josephine Dang <dangjos02@gmail.com>
2025-02-17 06:16:04 +0000

    C - script 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ * Return:
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
