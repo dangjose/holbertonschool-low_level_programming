3e361f583ff8b34ee3c91fbb6789aaae7d2cc410
Josephine Dang <dangjos02@gmail.com>
2025-02-18 03:51:19 +0000

    var-if-else-while task 0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: Print whether random number is positive or negative.
+ * Return: 0
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positivei\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
