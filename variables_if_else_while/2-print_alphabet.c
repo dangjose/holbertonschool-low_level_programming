a54b5bbbba58575e450f3a9a93972bdff769faa3
Josephine Dang <dangjos02@gmail.com>
2025-02-18 05:43:04 +0000

    var-if-else-while task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: Print the alphabet in lowercase
+ * Return: 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
