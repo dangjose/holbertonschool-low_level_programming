dc730163ce3e1fc6291486630df6077c6e57b9c5
Josephine Dang <dangjos02@gmail.com>
2025-02-19 07:46:05 +0000

    var-if-else-while task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: Print single digit numbers separated by ','
+ * Return: 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int num = 48;
+
+	while (num < 58)
+	{
+		putchar(num);
+		if (num == 57)
+		{
+			break;
+		}
+		putchar(44);
+		putchar(32);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
