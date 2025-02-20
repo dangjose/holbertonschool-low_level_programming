3fa3cdbbbb0788b1018ab3988ee3cf548f4e0dbc
Josephine Dang <dangjos02@gmail.com>
2025-02-20 07:54:15 +0000

    func-nestloops task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - 
+ *
+ * Description: Function that prints the alphabet in lowercase.
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+}
