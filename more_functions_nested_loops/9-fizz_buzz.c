3a1c2735e962298b5f7c125914c327c744c092ff
Josephine Dang <dangjos02@gmail.com>
2025-03-01 08:27:04 +0000

    fix spacing issues

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,14 +11,19 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 5 == 0 && i % 3 == 0)
-			printf("FizzBuzz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
-		else if (i % 3 == 0)
-			printf("Fizz ");
+		if (i == 1)
+			printf("%d", i);
 		else
-			printf("%d ", i);
+		{
+			if (i % 5 == 0 && i % 3 == 0)
+				printf(" FizzBuzz");
+			else if (i % 5 == 0)
+				printf(" Buzz");
+			else if (i % 3 == 0)
+				printf(" Fizz");
+			else
+				printf(" %d", i);
+		}
 	}
 	printf("\n");
 	return (0);
