1bc8656a50d83b315d674481d84a48b18f30424b
Josephine Dang <dangjos02@gmail.com>
2025-03-01 10:13:41 +0000

    point arr str task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Obtains length of string.
+ *
+ * @s: String in which length is being determined.
+ *
+ * Return: Length of s.
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
