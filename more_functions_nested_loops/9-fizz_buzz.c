e1678ccb3dede24d107beb9807eb70962316aee4
Josephine Dang <dangjos02@gmail.com>
2025-03-01 08:12:58 +0000

    more func nestloops task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 5 == 0 && i % 3 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
+
