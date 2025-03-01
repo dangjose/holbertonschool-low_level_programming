96f8e096acff422a4cf00e800c133210649dc873
Josephine Dang <dangjos02@gmail.com>
2025-03-01 11:33:41 +0000

    point arr str task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other char of str
+ *
+ * @str: String to be printed.
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
