5f0e92b164f3668fec7266933fe982e12cd83e13
Josephine Dang <dangjos02@gmail.com>
2025-03-02 08:09:14 +0000

    more point arr str task 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares 2 strings.
+ *
+ * @s1: String 1
+ * @s2: String 2
+ *
+ * Return:  
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+	return (0);
+
+}
