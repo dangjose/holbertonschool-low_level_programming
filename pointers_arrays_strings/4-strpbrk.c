85083eae0c1743e3b8de4f9c915140583b70cffd
Josephine Dang <dangjos02@gmail.com>
2025-03-04 01:25:37 +0000

    even more point arr str task 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - Finds the first match of any character from one string in another
+ *
+ * @s: String to be searched.
+ * @accept: Pointer that contains the characters to search for.
+ *
+ * Return: Pointer to the first match in s or NULL if no accept byte is found.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		s++;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s);
+			}
+		}
+	}
+	return (NULL);
+}
