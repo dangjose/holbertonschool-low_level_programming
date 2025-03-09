1d859d2ae6884fe1ea41dea29242baa88fb48ae7
Josephine Dang <dangjos02@gmail.com>
2025-03-09 07:06:10 +0000

    fix betty

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -11,14 +11,11 @@
 int factorial(int n)
 {
 	int result;
-	
+
 	if (n < 0)
 		return (-1);
 	else if (n == 1)
 		return (1);
-	else
-	{
-		result = (n * factorial(n - 1));
-		return (result);
-	}
+	result = (n * factorial(n - 1));
+	return (result);
 }
