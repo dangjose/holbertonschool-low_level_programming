1f6bbe90e18f33ed3a43b339daafbeda7fa2db9e
Josephine Dang <dangjos02@gmail.com>
2025-03-11 03:27:36 +0000

    fix loop error

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -24,10 +24,10 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i] != 0; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		s1_len++;
 
-	for (i = 0; s2[i] != 0; i++)
+	for (i = 0; s2[i] != '\0'; i++)
 		s2_len++;
 
 	len_total = s1_len + s2_len;
