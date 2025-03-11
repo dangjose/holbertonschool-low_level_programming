92dc48fb7478c5c4a0b719037783b70f52ab3287
Josephine Dang <dangjos02@gmail.com>
2025-03-11 02:26:20 +0000

    malloc free task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup - Duplicate string to new space in memory.
+ *
+ * @str: String to be duplicated
+ *
+ * Return: A pointer to memory with copied string, or NULL if it fails.
+ */
+
+char *_strdup(char *str)
+{
+	char *arr;
+	int i;
+	int length = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		length++;
+	}
+
+	arr = malloc(sizeof(*arr) * length);
+
+	if (length == 0)
+		return (NULL);
+	else if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < length; i++)
+	{
+		arr[i] = str[i];
+	}
+
+	return (arr);
+}
